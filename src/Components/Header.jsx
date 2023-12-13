c02c411720747dd51ca0bf36201b7b475b281281
Slav Mosoyan <vladanik1999@gmail.com>
2023-12-13 18:21:23 +0400

    add Header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <span className="Logo">Hose Staff</span>
+      </div>
+      <div className="presentation"></div>
+    </header>
+  );
+};
+
+export default Header;
